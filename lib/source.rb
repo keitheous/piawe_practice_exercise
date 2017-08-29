79d48636216b19aa3e2369006c58de814df4d74f
Keith Chong <keithchongwy@gmail.com>
2017-08-29 21:37:54 +1000

    source.rb and basic spec

--- lib/source.rb
+++ lib/source.rb
@@ -1,2 +1,9 @@
+require 'json'
+
 class Source
+  attr_accessor :people, :rules
+  def initialize(people = './sources/people.json', rules = './sources/people.json')
+    @people = JSON.parse(File.read(people))
+    @rules = JSON.parse(File.read(rules))
+  end
 end
--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 require 'source'
 
 RSpec.describe Source do
-  it 'has both source for rule and people' do
-  end
-
-  it 'contain data  and is not an empty source' do
+  it 'has default rule and people' do
+    source = Source.new
+    expect(source.rules.class).to be(Hash)
+    expect(source.people.class).to be(Hash)
   end
 end
