0055b645e037a60fdd88bf87c94910bcad6bb295
Keith Chong <keithchongwy@gmail.com>
2017-08-29 21:07:48 +1000

    finished durator.rb

--- lib/durator.rb
+++ lib/durator.rb
@@ -1,10 +1,20 @@
+require 'date'
+
 class Durator
   attr_accessor :upper_limit, :lower_limit
   def initialize(lower_limit, upper_limit)
-    @lower_limit = lower_limit
-    @upper_limit = upper_limit
+    @lower_limit = lower_limit if date_format?(lower_limit)
+    @upper_limit = upper_limit if date_format?(upper_limit)
   end
 
-  def valid?
+  def weeks_apart
+    days_apart = (Date.parse(@lower_limit).mjd - Date.parse(@upper_limit).mjd).abs + 1
+    weeks_apart = days_apart / 7
+  end
+
+  def date_format?(input)
+    true if Date.parse(input)
+  rescue
+    false
   end
 end
--- spec/durator_spec.rb
+++ spec/durator_spec.rb
@@ -3,23 +3,26 @@ require 'durator'
 require 'date'
 
 RSpec.describe Durator do
-  it 'initialize accepts two params as upper and lower limits' do
-    # durator = Durator.new('A', 'B')
-    # expect(durator.lower_limit).to eq('A');
-    # expect(durator.upper_limit).to eq('B')
+  it 'initialize accepts 2 dates params' do
+    invalid_durator = Durator.new('A', '3')
+    valid_durator = Durator.new('1/2/2013', '2013/12/1')
+    expect(invalid_durator.lower_limit).to eq(nil)
+    expect(invalid_durator.upper_limit).to eq(nil)
+    expect(valid_durator.lower_limit).to eq('1/2/2013')
+    expect(valid_durator.upper_limit).to eq('2013/12/1')
   end
 
   it 'validates inputs respond to Date format' do
-    # expect respondto(Date?)
-    # expect different format - to work?
-  end
-
-  it 'calculates duration between upper and lower limits in days' do
-    # expect upper_limit > lower_limit
-    # expect lower_limit > upper_limit
+    skip
+    # dont know how to do this
   end
 
   it 'calculates the number of weeks between upper and lower limits' do
-    # expect /7
+    first_pair_dates= Durator.new('1/1/2013', '14/1/2013')
+    second_pair_dates= Durator.new('1/1/2013', '26/1/2013')
+    third_pair_dates = Durator.new('1/1/2013', '3/1/2013')
+    expect(first_pair_dates.weeks_apart).to eq(2)
+    expect(second_pair_dates.weeks_apart).to eq(3)
+    expect(third_pair_dates.weeks_apart).to eq(0)
   end
 end
